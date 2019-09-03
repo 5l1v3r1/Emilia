6eab32a4227a7756757e820c68fd56b4ab0abc74
Mark Wetter <mark@Marks-MBP.fritz.box>
2019-09-03 23:07:48 +0200

    fix typo

--- discord/cmds/games/coinflip.go
+++ discord/cmds/games/coinflip.go
@@ -9,15 +9,15 @@ import (
 )
 
 func Coinflip(ds *discordgo.Session, dm *discordgo.Message, args []string) {
-	answers := []string{"heads", "tails"}
+	answers := []string{"head", "tails"}
 	n := rand.Intn(2)
 	if len(args) == 0 {
-		ds.ChannelMessageSend(dm.ChannelID, "Tails or Heads?")
+		ds.ChannelMessageSend(dm.ChannelID, "Tails or head?")
 		return
 	}
 	PlayerChoice := args[0]
 	PlayerChoice = strings.ToLower(PlayerChoice)
-	if PlayerChoice == "tails" || PlayerChoice == "heads" {
+	if PlayerChoice == "tails" || PlayerChoice == "head" {
 		ai := answers[n]
 		if PlayerChoice == ai {
 			ret := "You won! Gratulations. Added 50 coins to your wallet."
@@ -28,7 +28,7 @@ func Coinflip(ds *discordgo.Session, dm *discordgo.Message, args []string) {
 			ds.ChannelMessageSend(dm.ChannelID, "You lost")
 		}
 	} else {
-		ds.ChannelMessageSend(dm.ChannelID, "You have to select between 'heads' and 'tails'")
+		ds.ChannelMessageSend(dm.ChannelID, "You have to select between 'head' and 'tails'")
 	}
 
 }
